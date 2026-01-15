a586711933de50260cbb24b72ec511b6cdfebe5f
preconet-nxtcall <nxtcall.app@gmail.com>
2026-01-15 15:32:46 +0530

    Remove pages_read_engagement permission

--- frontend/admin/js/facebook.js
+++ frontend/admin/js/facebook.js
@@ -74,7 +74,7 @@ class FacebookManager {
                 console.log("User cancelled login or did not fully authorize.");
             }
         }, {
-            scope: 'pages_show_list,leads_retrieval,pages_read_engagement',
+            scope: 'pages_show_list,leads_retrieval',
             auth_type: 'reauthenticate',
             return_scopes: true
         });
